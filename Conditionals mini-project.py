ddbd677f6115edd26f89ceecb32fb85056209e5a
Mili Visnjic <milivisnjic@yahoo.com>
2024-07-12 15:09:22 -0400

    Add files via upload

---
+++ Conditionals mini-project.py
@@ -0,0 +1,11 @@
+for price in range(5):
+    if price < 5:
+        print("I can pay", price)
+    else:
+        print("No deal")
+
+        # output: I can pay 0
+                  I can pay 1
+                  I can pay 2
+                  I can pay 3
+                  I can pay 4
